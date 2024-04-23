ac8a49f91db882cf0db8324ba3bfec3ab230df8f
JDeepankar <167373914+JDeepankar@users.noreply.github.com>
2024-04-23 22:58:07 +0530

    Create SolidRhombusPattern.java

---
+++ SolidRhombusPattern.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class SolidRhombus {
+    public static void main(String[] args) {
+        for (int i = 5; i >=1 ; i--) {
+            for (int j = 1; j <= i-1 ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=4 ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
